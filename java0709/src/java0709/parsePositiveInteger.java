985387c4655935447e243291fe36e0909f54ee94
ChangHyun <19941128o@gmail.com>
2024-07-09 17:20:19 +0900

    java0709 file upload

--- java0709/src/java0709/parsePositiveInteger.java
+++ java0709/src/java0709/parsePositiveInteger.java
@@ -1,8 +1,30 @@
 package java0709;
 
+import java.util.Scanner;
 
 public class parsePositiveInteger {
-	public static void main(String[] args) {
+	public static int getPositiveNumber(int number) {
+		if(number > 0) {
+			return number;
+		}else {
+			return number * -1;
+		}
+	}
+	
+	public static int getNumber(String msg,Scanner sc) {
+		System.out.print(msg);
+		String input = sc.nextLine();
+		return Integer.parseInt(input);
 		
+	}
+	public static void main(String[] args) {
+		int number = 0;
+		int result = 0;
+		
+		Scanner sc = new Scanner(System.in);
+		number = getNumber("입력할 숫자 : ",sc);
+		
+		result = getPositiveNumber(number);
+		System.out.printf("숫자는 : %d 입니다.",result);
 	}
 }
