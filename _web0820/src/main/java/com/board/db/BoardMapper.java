88656253e3e084676b5d188f339fc5a08d0f59f5
ChangHyun <19941128o@gmail.com>
2024-08-20 17:48:04 +0900

    java file 20240820

---
+++ _web0820/src/main/java/com/board/db/BoardMapper.java
@@ -0,0 +1,10 @@
+package com.board.db;
+
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Select;
+
+@Mapper
+public interface BoardMapper {
+	@Select("select count(*) from dept")
+	int countBoard();
+}
